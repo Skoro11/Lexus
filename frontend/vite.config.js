1a74edd5782d5e5ddfaf8781039256296eaf6a50
Toni <skoro.toni01@gmail.com>
2025-05-23 12:11:42 +0200

    Proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,23 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { path } from 'path';
+
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), 'VITE_');
 
 export default defineConfig(({ mode }) => {
-  // Load environment variables based on the current mode (development, production, etc)
-  const env = loadEnv(mode, process.cwd(), '')
+  // Load env variables for current mode (development, production, etc)
+
 
   return {
     plugins: [react()],
     server: {
-      proxy: {
-        // Use env variable here
-        '/api': env.VITE_BASE_PROXY
-      }
-    }
+        proxy: {
+            '/api': {
+                target: env.VITE_BASE_PROXY, // Use the environment variable
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
   }
 })
