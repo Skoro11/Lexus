164f13b6b0f44010691d987ce59e344a90c7db4a
Toni <skoro.toni01@gmail.com>
2025-06-24 16:42:29 +0200

    Reset backend folder to fc1828a

--- backend/routes/LikeListRoutes.js
+++ backend/routes/LikeListRoutes.js
@@ -1,13 +1,13 @@
 import {Router} from "express"
 
-import { addToLikeList, findItemsById, getLikeList, removeAllFromLikeList, removeFromLikeList } from "../controllers/likelistController.js"
+import { addToLikeList, findItemsById, getLikeList, removeAllFromLikeList, removeFromLikeList } from "../controllers/LikeLisTController.js"
 
 const router = Router();
 
 router.post("/add",addToLikeList)
 router.post("/remove",removeFromLikeList)
 router.post("/remove/all",removeAllFromLikeList)
-router.get("/get",getLikeList)
+router.get("/get/likelist",getLikeList)
 router.post("/id",findItemsById)
 
 export default router;
