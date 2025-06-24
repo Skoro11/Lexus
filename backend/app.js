c6e5f32c180f01def600c1b491ff841394174215
Toni <skoro.toni01@gmail.com>
2025-06-24 15:33:13 +0200

    Change name

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,36 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import authRoutes from "./routes/authRoutes.js"
-import cartRoutes from "./routes/cartRoutes.js"
-import productRoutes from "./routes/productRoutes.js"
-import cookieParser from "cookie-parser"
-import LikeListRoutes from "./routes/likeListRoutes.js"
-import WatchlistRoutes from "./routes/watchlistRoutes.js"
-import AdminRoutes from "./routes/adminRoutes.js"
-import { authenticateToken } from "./middleware/TokenVerification.js"
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import cookieParser from "cookie-parser";
+import LikeListRoutes from "./routes/LikeListRoutes.js";
+import WatchlistRoutes from "./routes/watchlistRoutes.js";
+import AdminRoutes from "./routes/adminRoutes.js";
+import { authenticateToken } from "./middleware/TokenVerification.js";
+dotenv.config();
 const app = express();
-const port= process.env.PORT
+const port = process.env.PORT;
 
+app.use(express.json());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://lexus-vite-git-main-skoro11s-projects.vercel.app",
+      `https://www.lexusshop.app`,
+    ],
+    credentials: true,
+  })
+);
 
-
-app.use(express.json())
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://lexus-vite-git-main-skoro11s-projects.vercel.app',`https://www.lexusshop.app`],
-  credentials: true,
-}));
-
-
-app.use(cookieParser())
-app.use("/api/auth",authRoutes)
-app.use("/api/cart",authenticateToken,cartRoutes)
-app.use("/api/product",productRoutes)
-app.use("/api/likelist",authenticateToken,LikeListRoutes)
-app.use("/api/watchlist",authenticateToken, WatchlistRoutes)
-app.use("/admin",AdminRoutes)
-
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/cart", authenticateToken, cartRoutes);
+app.use("/api/product", productRoutes);
+app.use("/api/likelist", authenticateToken, LikeListRoutes);
+app.use("/api/watchlist", authenticateToken, WatchlistRoutes);
+app.use("/admin", AdminRoutes);
 
 export default app;
