fada0126fc889c8979524f012bace611496992fa
Toni Skoro <skoro.toni01@gmail.com>
2025-07-03 00:01:13 +0200

    Likepage and Watchlist improved

--- frontend/src/pages/AllProductPage.jsx
+++ frontend/src/pages/AllProductPage.jsx
@@ -67,7 +67,7 @@ function AllProductsPage() {
           </div>
         </div>
 
-        <div className=" grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-6">
+        <div className=" grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-6 mb-10">
           {shouldRender &&
             allProduct.map((product) => (
               <div key={product._id}>
